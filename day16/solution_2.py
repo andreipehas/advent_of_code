17f42e0720b396b8f67800c759e6cf85d0bb1a32
andreipehas <andrei.prahoveanu22@gmail.com>
2023-12-18 12:32:18 +0000

    day 16

---
+++ day16/solution_2.py
@@ -0,0 +1,88 @@
+from enum import Enum
+from contextlib import redirect_stdout
+from pathlib import Path
+from typing import List
+
+INPUT_NAME = "input.txt"
+
+class Direction(Enum):
+	NORTH = (-1, 0)
+	SOUTH = ( 1, 0)
+	EAST  = ( 0, 1)
+	WEST  = ( 0,-1)
+
+NEXT_COORDINATES = {
+	'.': {x: [x] for x in Direction},
+	'\\': {
+		Direction.NORTH: [Direction.WEST],
+		Direction.SOUTH: [Direction.EAST],
+		Direction.EAST: [Direction.SOUTH],
+		Direction.WEST: [Direction.NORTH],
+	},
+	'/': {
+		Direction.NORTH: [Direction.EAST],
+		Direction.SOUTH: [Direction.WEST],
+		Direction.WEST: [Direction.SOUTH],
+		Direction.EAST: [Direction.NORTH],
+	},
+	'|': {
+		Direction.NORTH: [Direction.NORTH],
+		Direction.SOUTH: [Direction.SOUTH],
+		Direction.EAST: [Direction.NORTH, Direction.SOUTH],
+		Direction.WEST: [Direction.NORTH, Direction.SOUTH],
+	},
+	'-': {
+		Direction.NORTH: [Direction.EAST, Direction.WEST],
+		Direction.SOUTH: [Direction.EAST, Direction.WEST],
+		Direction.EAST: [Direction.EAST],
+		Direction.WEST: [Direction.WEST],
+	},
+}
+
+def valid_coordinates(x: int, y: int, len_x: int, len_y: int) -> bool:
+   return 0 <= x and x < len_x and 0 <= y and y < len_y
+
+def traverse(lines: List[str], x: int, y: int, start_direction: Direction) -> int:
+    explore_queue = [(x, y, start_direction)]
+    visited = [[None] * len(lines[0]) for _ in range(len(lines))]
+    sol = 0
+    
+    while explore_queue:
+        x, y, current_dir = explore_queue.pop()
+        if not visited[x][y]:
+            sol += 1
+            visited[x][y] = []
+        if not current_dir in visited[x][y]:
+            visited[x][y].append(current_dir)
+            for next_dirs in NEXT_COORDINATES[lines[x][y]][current_dir]:
+                if valid_coordinates(
+                    x + next_dirs.value[0],
+                    y + next_dirs.value[1], 
+                    len(lines[0]), len(lines)
+                ):
+                    explore_queue.append(
+                        (x + next_dirs.value[0], y + next_dirs.value[1], next_dirs)
+                    )
+
+    return sol
+				
+
+if __name__ == "__main__":
+    script_dir = Path(__file__).resolve().parent
+    file_path = script_dir / INPUT_NAME
+
+    sol = 0
+
+    with open(script_dir / 'output.txt', 'w') as f:
+        with redirect_stdout(f):
+            with open(file_path, 'r') as file:
+                lines = [list(line.rstrip()) for line in file]
+                for i in range(len(lines)):
+                    sol = max(sol, traverse(lines, i, 0, Direction.EAST))
+                    sol = max(sol, traverse(lines, i, len(lines[0]) - 1, Direction.WEST))
+                
+                for i in range(len(lines[0])):
+                    sol = max(sol, traverse(lines, 0, i, Direction.SOUTH))
+                    sol = max(sol, traverse(lines, len(lines) - 1, i, Direction.NORTH))
+                    
+            print(f"------> {sol}")
